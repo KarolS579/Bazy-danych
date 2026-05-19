d9c683bee3643a8e90d566b3c7421460f9e1cc2f
volod <volodiaromaniak2@gmail.com>
2026-05-19 03:54:47 +0200

    Dodanie potwierdzenia maila nowego użytkownika przez skrzynkę pocztową

---
+++ Bazy danych/App_Start/IdentityConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace Bazy_danych
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            var host = ConfigurationManager.AppSettings["MailtrapHost"];
+            var port = int.Parse(ConfigurationManager.AppSettings["MailtrapPort"]);
+            var user = ConfigurationManager.AppSettings["MailtrapUser"];
+            var pass = ConfigurationManager.AppSettings["MailtrapPass"];
+            var from = ConfigurationManager.AppSettings["MailtrapFrom"];
+
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(user, pass),
+                EnableSsl = true
+            };
+
+            var mail = new MailMessage(from, message.Destination)
+            {
+                Subject = message.Subject,
+                Body = message.Body,
+                IsBodyHtml = true
+            };
+
+            return client.SendMailAsync(mail);
+        }
+    }
+}
--- Bazy danych/Migrations/Configuration.cs
+++ Bazy danych/Migrations/Configuration.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
 using Bazy_danych.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -18,11 +16,8 @@ namespace Bazy_danych.Migrations
 
         protected override void Seed(Bazy_danych.Models.ApplicationDbContext context)
         {
-            // Initialize the user and role managers
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-            // 1. Create the Admin and User roles if they don't exist
             if (!roleManager.RoleExists("Admin"))
             {
                 roleManager.Create(new IdentityRole("Admin"));
@@ -31,44 +26,6 @@ namespace Bazy_danych.Migrations
             if (!roleManager.RoleExists("User"))
             {
                 roleManager.Create(new IdentityRole("User"));
-            }
-
-            // 2. Load admin users from AdminUsers.json configuration file
-            var adminsFromConfig = AdminConfigLoader.LoadAdmins();
-
-            // 3. Create admin users from the configuration file
-            foreach (var adminConfig in adminsFromConfig)
-            {
-                if (string.IsNullOrWhiteSpace(adminConfig.Email) || string.IsNullOrWhiteSpace(adminConfig.Password))
-                {
-                    continue; // Skip if email or password is empty
-                }
-
-                var user = userManager.FindByEmail(adminConfig.Email);
-                if (user == null)
-                {
-                    user = new ApplicationUser
-                    {
-                        UserName = adminConfig.Email,
-                        Email = adminConfig.Email
-                    };
-
-                    var result = userManager.Create(user, adminConfig.Password);
-
-                    // Assign the user to the Admin role
-                    if (result.Succeeded)
-                    {
-                        userManager.AddToRole(user.Id, "Admin");
-                    }
-                }
-                else
-                {
-                    // If user exists but isn't in the Admin role, add them
-                    if (!userManager.IsInRole(user.Id, "Admin"))
-                    {
-                        userManager.AddToRole(user.Id, "Admin");
-                    }
-                }
             }
         }
     }
