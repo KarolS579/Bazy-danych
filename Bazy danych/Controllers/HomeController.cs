bdd90bacfa7bd52b342267485ee9118bea92c109
Romanyak_Volodymyr”git config --global user.email volodiaromaniak2@gmail.com” <volodiaromaniak2@gmail.com>
2026-06-11 00:32:05 +0200

    Dodanie WarehouseControllera i przeniesienie tej strony do oddzielnego folderu

--- Bazy danych/Controllers/HomeController.cs
+++ Bazy danych/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -131,101 +131,6 @@ namespace Bazy_danych.Controllers
             db.SaveChanges();
 
             return RedirectToAction("Equipment");
-        }
-
-        public ActionResult Warehouses()
-        {
-            var warehouses = db.Magazyny.OrderByDescending(x => x.CreatedDate).ToList();
-            return View(warehouses);
-        }
-
-        public ActionResult AddWarehouse()
-        {
-            return View(new Magazyn());
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult AddWarehouse(Magazyn model)
-        {
-            if (ModelState.IsValid)
-            {
-                if (model.ZajeteMiejsce > model.Pojemnosc)
-                {
-                    ModelState.AddModelError("ZajeteMiejsce", "Zajęte miejsce nie może być większe niż pojemność.");
-                    return View(model);
-                }
-
-                model.CreatedDate = DateTime.Now;
-                db.Magazyny.Add(model);
-                db.SaveChanges();
-                return RedirectToAction("Warehouses");
-            }
-
-            return View(model);
-        }
-
-        public ActionResult EditWarehouse(int? id)
-        {
-            if (!id.HasValue)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            var warehouse = db.Magazyny.Find(id.Value);
-            if (warehouse == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(warehouse);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult EditWarehouse(Magazyn model)
-        {
-            if (ModelState.IsValid)
-            {
-                if (model.ZajeteMiejsce > model.Pojemnosc)
-                {
-                    ModelState.AddModelError("ZajeteMiejsce", "Zajęte miejsce nie może być większe niż pojemność.");
-                    return View(model);
-                }
-
-                var warehouse = db.Magazyny.Find(model.Id);
-                if (warehouse == null)
-                {
-                    return HttpNotFound();
-                }
-
-                warehouse.Nazwa = model.Nazwa;
-                warehouse.Lokalizacja = model.Lokalizacja;
-                warehouse.Pojemnosc = model.Pojemnosc;
-                warehouse.ZajeteMiejsce = model.ZajeteMiejsce;
-                warehouse.Status = model.Status;
-
-                db.SaveChanges();
-                return RedirectToAction("Warehouses");
-            }
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteWarehouse(int id)
-        {
-            var warehouse = db.Magazyny.Find(id);
-            if (warehouse == null)
-            {
-                return HttpNotFound();
-            }
-
-            db.Magazyny.Remove(warehouse);
-            db.SaveChanges();
-
-            return RedirectToAction("Warehouses");
         }
 
         public ActionResult Customers()
